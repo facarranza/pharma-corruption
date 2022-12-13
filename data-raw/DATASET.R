0477e0b434ddd15961995ce1f176583e64e6322f
CamilaAchuri <camilaachury.4@gmail.com>
2022-12-13 15:51:45 -0300

    add dic to data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,5 +5,12 @@ library(jsonlite)
 # ls <- lapply(readLines("data-raw/data/aimon.json"), fromJSON)
 
 data_pharma <- stream_in(file("data-raw/data/aimon.json"))
-data_pharma$`Published-at` <- lubridate::as_date(lubridate::ymd_hms(data_pharma$`Published Date`))
+data_pharma$`Published-at` <- lubridate::as_date(
+  lubridate::ymd_hms(data_pharma$`Published Date`))
 usethis::use_data(data_pharma, overwrite = TRUE)
+
+id_names <- tolower(paste0("id_",
+                           gsub("\\.", "_",make.names(names(data_pharma)))))
+class_var <- purrr::map(data_pharma, class) |> unlist()
+dic_pharma <- dplyr::tibble(id = id_names, label = names(data_pharma), ftype = class_var)
+usethis::use_data(dic_pharma, overwrite = TRUE)
