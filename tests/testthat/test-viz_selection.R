3d471c672026eabf1d8888d08d7b1ad8bdd7e239
CamilaAchuri <camilaachury.4@gmail.com>
2022-12-13 22:18:04 -0300

    add data aggregation in viz selection test

--- tests/testthat/test-viz_selection.R
+++ tests/testthat/test-viz_selection.R
@@ -1,5 +1,6 @@
 test_that("Viz selection", {
   data_viz <- variable_selection(data = data_pharma, viz = "map")
+  data_viz  <- var_aggregation(data_viz, Total = dplyr::n())
   data_result <- viz_selection(data_viz, dic_pharma, "map")
   expect_equal(data_result, "lfltmagic::lflt_choropleth_GnmNum")
 })
