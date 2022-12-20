894dc8383909a9be1afd52af9b1c1c76a5fd6c42
CamilaAchuri <camilaachury.4@gmail.com>
2022-12-20 01:01:41 -0300

    filter all in function

--- R/00_data_filter.R
+++ R/00_data_filter.R
@@ -10,23 +10,25 @@ data_filter <- function(data, dic, var_inputs, .id) {
     purrr::map(function(.x) {
       if (!is.null(var_inputs[[.x]])) {
         if (!setequal(var_inputs[[.x]], "")) {
-          name_var <- names(var_inputs)[.x]
-          info_var <- dic |>
-            dplyr::filter(id %in% name_var)
-          filter_var <- var_inputs[[.x]]
-          if (info_var$ftype == "Date") {
-            df <<- filter_dates(df, range_date = filter_var, by = info_var$label)
-          }
-          if (info_var$ftype == "list") {
-            df <<- filter_list(df, filter_var, info_var$label, .id = .id)
-          }
-          if (info_var$ftype == "character") {
-            df <<- df |>
-              dplyr::filter(!!dplyr::sym(info_var$label) %in% filter_var)
-          }
-          if (info_var$ftype == "numeric") {
-            df <<- df |>
-              dplyr::filter(!!dplyr::sym(info_var$label) == filter_var)
+          if (!setequal(var_inputs[[.x]], "All")) {
+            name_var <- names(var_inputs)[.x]
+            info_var <- dic |>
+              dplyr::filter(id %in% name_var)
+            filter_var <- var_inputs[[.x]]
+            if (info_var$ftype == "Date") {
+              df <<- filter_dates(df, range_date = filter_var, by = info_var$label)
+            }
+            if (info_var$ftype == "list") {
+              df <<- filter_list(df, filter_var, info_var$label, .id = .id)
+            }
+            if (info_var$ftype == "character") {
+              df <<- df |>
+                dplyr::filter(!!dplyr::sym(info_var$label) %in% filter_var)
+            }
+            if (info_var$ftype == "numeric") {
+              df <<- df |>
+                dplyr::filter(!!dplyr::sym(info_var$label) == filter_var)
+            }
           }
         }
       }
